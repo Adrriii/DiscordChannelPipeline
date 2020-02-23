8617c8ddff93406045a0826ee04dffc79924291b
Adri <adrien.boitelle@gmail.com>
2020-02-23 11:56:23 +0100

    nouvelle approach

--- listen/index.js
+++ listen/index.js
@@ -1,15 +1,11 @@
 module.exports = {
-  start: function (output) {
+  start: function () {
 
     const Discord = require('discord.js');
     const { Readable } = require('stream');
     const fs = require('fs');
     var auth = require('./auth.json');  
     const client = new Discord.Client();
-    
-    client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
-    });
 
     // make a new stream for each time someone starts to talk
     function generateOutputFile(channel, member) {
@@ -18,36 +14,13 @@ module.exports = {
       return fs.createWriteStream(fileName);
     }
     
-    client.on('message', msg => {
-      // Voice only works in guilds, if the message does not come from a guild,
-      // we ignore it
-      if (!msg.guild) return;
-    
-      if (msg.content === 'ecoute moi') {
-        // Only try to join the sender's voice channel if they are in one themselves
-        if (msg.member.voiceChannel) {
-          channel = 680915973360058414;
-          voiceChannel = msg.member.voiceChannel
-            voiceChannel.join()
-            .then(conn => { // Connection is an instance of VoiceConnection
-              // create our voice receiver
-              const receiver = conn.createReceiver();      
-
-              conn.on('speaking', (user, speaking) => {
-                if (speaking) {
-                  // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
-                  const audioStream = receiver.createPCMStream(user);
-                  // pipe our audio data into the file stream
-                  audioStream.pipe(output);
-                  output.on('data', console.log);
-                }
-              });
-            })
-            .catch(console.log);
-        } else {
-          msg.reply('t pas en voc');
-        }
-      }
+    client.on('ready', msg => {
+      voiceChannel = client.channels.get("680915973360058414")
+        voiceChannel.join()
+        .then(conn => { // Connection is an instance of VoiceConnection
+          client.emit('connected', conn);
+        })
+        .catch(console.log);
     });
     
     // listen
--- speak/index.js
+++ speak/index.js
@@ -1,35 +1,29 @@
 
 
 module.exports = {
-  start: function (input) {
+  start: function (listener) {
     
     const Discord = require('discord.js');
     const client = new Discord.Client();
-    var auth = require('./auth.json'); 
+    var auth = require('./auth.json');
     
-    client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
-    });
-    
-    client.on('message', msg => {
-      // Voice only works in guilds, if the message does not come from a guild,
-      // we ignore it
-      if (!msg.guild) return;
-    
-      if (msg.content === 'entendre') {
-        // Only try to join the sender's voice channel if they are in one themselves
-        if (msg.member.voiceChannel) {
-          channel = 680916063311102207;
-          msg.member.voiceChannel.join()
-            .then(connection => { // Connection is an instance of VoiceConnection
+    client.on('ready', msg => {
+      voiceChannel = client.channels.get("680915973360058414") // 680916063311102207
+        voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          
+          const receiver = listener.createReceiver();      
+
+          listener.on('speaking', (user, speaking) => {
+            if (speaking) {
+              // this creates a 16-bit signed PCM, stereo 48KHz PCM stream.
+              const audioStream = receiver.createOpusStream(user);
               
-              connection.playStream(input);
-            })
-            .catch(console.log);
-        } else {
-          msg.reply('t pas en voc bro');
-        }
-      }
+              connection.playOpusStream(audioStream);
+            }
+          });
+        })
+        .catch(console.log);
     });
     
     client.login(auth["token"]);
